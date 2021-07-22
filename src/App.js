dabe71373bd97509a473230c4c9a3e425f8c2875
Michal Jarosz <michal.jarosz@hotmail.com>
2021-07-22 21:40:59 +0100

    testing-library fake timers issue repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = React.useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <button
+        onClick={async () => {
+          await new Promise((resolve) => setTimeout(() => resolve(), 500));
+          const response = await fetch(
+            "https://api.github.com/users/mjetek"
+          ).then((r) => r.json());
+          setName(response.name);
+        }}
+      >
+        Fetch
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent, act } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+afterEach(() => {
+  jest.runOnlyPendingTimers();
+  jest.useRealTimers();
+});
+
+function toJSONBlob(obj) {
+  return new Blob([JSON.stringify(obj)], {
+    type: "application/json;charset=utf-8",
+  });
+}
+
+test("renders learn react link", async () => {
+  jest
+    .spyOn(window, "fetch")
+    .mockResolvedValue(new Response(toJSONBlob({ name: "Michal" })));
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  fireEvent.click(screen.getByRole("button", { name: /fetch/i }));
+  act(() => {
+    jest.advanceTimersByTime(300);
+  });
+
+  await screen.findByRole("heading", { name: /michal/i });
 });
